85e523f65b4ede4d04ee09960efb020cb1700dda
Tom Austin <tom.austin@streets-heaver.com>
2018-05-10 21:10:34 +0100

    init

---
+++ Metricity.Data/Entities/Metric.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metricity.Data.Entities
+{
+    public class Metric
+    { 
+        public int MetricId { get; set; }
+        public string AppliactionName { get; set; }
+        public string MethodName { get; set; }
+        public TimeSpan Duration { get; set; }
+    }
+}
---
+++ Metricity.Data/MetricityContext.cs
@@ -0,0 +1,20 @@
+﻿using Metricity.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metricity.Data
+{
+    public class MetricityContext : DbContext
+    {
+        public MetricityContext() : base ("Server=tcp:metricitydb.database.windows.net,1433;Initial Catalog=MetricityDB;Persist Security Info=False;User ID=tom;Password=oD%77Kk28KOXQF>gRW+r;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;")
+        {
+
+        }
+
+        public virtual DbSet<Metric> Metrics { get; set; }
+    }
+}

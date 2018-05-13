e649dc4e862e9952badf4815f04aa64fd7ab67ef
Tom Austin <tom.austin@streets-heaver.com>
2018-05-13 21:33:52 +0100

    Added examples project

---
+++ Examples/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunExamples();
+        }
+
+        static async void RunExamples()
+        {
+            //Returns time taken for execution of synchronous code
+            var syncTime = Metricity.Timings.Time(() =>
+            {
+                SyncMethod();
+            });
+
+            //Returns time taken for execution of asynchronous code
+            var asyncTime = Metricity.Timings.Time(async () =>
+            {
+                await ASyncMethod();
+            });
+
+            //Returns current memory usage of application
+            var memoryUsage = Metricity.Diagnostics.GetMemoryUsage();
+
+            //Returns current cpu usage of application
+            var cpuUsage = Metricity.Diagnostics.GetCPUUsage();
+
+            //Returns difference in memory usage from before synchronous code execution and after
+            var memoryChangeSync = Metricity.Diagnostics.GetMemoryChange(() =>
+            {
+                SyncMethod();
+            });
+
+            //Returns difference in memory usage from before asynchronous code execution and after
+            var memoryChangeASync = Metricity.Diagnostics.GetMemoryChange(async () =>
+            {
+                await ASyncMethod();
+            });
+
+            //Logs time taken for execution of synchronous code to database
+            Metricity.RemoteLog.Log(() =>
+            {
+                SyncMethod();
+            });
+
+            //Logs time taken for execution of asynchronous code to database
+            await Metricity.RemoteLog.Log(async () =>
+            {
+                await ASyncMethod();
+            });
+
+            //Logs time taken for execution of synchronous code to cache, cache is then commited to database when CommitCache is called
+            Metricity.RemoteLog.CacheLog(() =>
+            {
+                SyncMethod();
+            });
+
+            //Logs time taken for execution of synchronous code to cache, cache is then commited to database when CommitCache is called
+            await Metricity.RemoteLog.CacheLog(async () =>
+            {
+                await ASyncMethod();
+            });
+
+            //Clears all metrics from the cache
+            Metricity.RemoteLog.ClearCache();
+
+            //Returns the current cache
+            var cache = Metricity.RemoteLog.GetCache();
+
+            //Increments the counter by one
+            Metricity.Counters.Increment("counter");
+
+            //Decrements the counter by one
+            Metricity.Counters.Decrement("counter");
+
+            //Returns the count of the specified counter
+            Metricity.Counters.GetCurrentCount("counter");
+
+            //Resets the specified counter
+            Metricity.Counters.ClearCounter("counter");
+
+            //Resets all counters
+            Metricity.Counters.PurgeCounters();
+
+            //Gets the percentage splits of subset counters
+            var splits = Metricity.Counters.GetSubsetSplit("counter");
+        }
+
+        static void SyncMethod()
+        {
+            Thread.Sleep(1000);
+        }
+
+        static async Task ASyncMethod()
+        {
+            await Task.Run(() => Thread.Sleep(1000));
+        }
+    }
+}
